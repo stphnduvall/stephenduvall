c625b475982d21c7f262bb55e73f37ed1bd29892
Stephen DuVall <stephen.duvall13@gmail.com>
2018-04-09 22:26:06 -0400

    Wrote code that prints diff of the files after compilation

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var chalk = require('chalk')
+var JsDiff = require('diff')
 var sass = require('node-sass')
 var fs = require('fs')
 sass.render({
@@ -5,8 +7,19 @@ sass.render({
 }, function(err, result) {
 
   if (err === null) {
+    printDiff(JsDiff.diffCss(fs.readFileSync("sass/main.css").toString("utf-8"), result.css.toString()))
     fs.writeFileSync('sass/main.css', result.css)
   } else {
     console.error(err)
   }
 })
+
+function printDiff(diff) {
+  diff.forEach((part) => {
+    if (part.added) {
+      console.log(chalk.green('%s'), "+ " + part.value)
+    } else if (part.removed) {
+      console.log(chalk.red('%s'), "- " + part.value)
+    }
+  })
+}
