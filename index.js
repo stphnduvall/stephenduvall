9848e4d3529d22295e6c61182551c969ca8ddff0
Stephen DuVall <stephen.duvall13@gmail.com>
2018-04-09 20:08:14 -0400

    Fixed index.js to log errors

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ sass.render({
   file: 'sass/main.scss'
 }, function(err, result) {
 
-  if (err === undefined) {
+  if (err === null) {
     fs.writeFileSync('sass/main.css', result.css)
   } else {
-    console.log('Could not handle your sass')
+    console.error(err)
   }
 })
