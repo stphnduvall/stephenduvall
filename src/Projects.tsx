b01afd11eaac52998f7969362701eebb40338b20
Unknown <stephen.duvall13@gmail.com>
2019-09-15 16:44:40 -0400

    Added content

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -12,7 +12,8 @@ export default function Projects() {
   let projectArray: project[] = [
     {title: 'Dad Joke Bot', img: 'http://stbartholomewingoldsby.weebly.com/uploads/3/2/0/8/32082737/9507512.jpg?250', desc: 'Who doesn\'t love a good dad joke every day? That\'s exactly what this project does. Built with typescript and NodeJs', link: 'https://github.com/stphnduvall/groupme-bot'},
     {title: 'MCStatus', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZhssWQs2hoInBs_5gU4Y5XVmfLUDoDLIfhuoebNEkNki2A-U6BA', desc: 'Simple npm module that checks the status of a Minecraft server. (Will be implemented on this site soon)', link: 'https://github.com/stphnduvall/mcstatus'},
-    {title: 'Roots Next Generation', img: 'https://www.rootsnextgeneration.org/roots.png', desc: 'A Podcast CMS for my local college ministry to record and upload podcasts ', link: 'https://rootsnextgeneration.org'},
+    {title: 'Roots Next Generation', img: 'https://www.rootsnextgeneration.org/roots.png', desc: 'A Podcast CMS for my local college ministry to record and upload podcasts', link: 'https://rootsnextgeneration.org'},
+    {title: 'movie-dl', img: 'https://www.rootsnextgeneration.org/roots.png', desc: 'A little bit of code that searches and downloads movies from a website', link: '#'},
   ]
 
   return (
